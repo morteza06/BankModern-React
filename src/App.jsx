388e5bc4f43c809757dfd9745ee34bc496cceb8b
morteza <mgahangir2006@gmail.com>
2023-03-01 16:56:45 +0330

    Config_Assets-Constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline bg-blue-gradient">
+      Hello world!
+    </h1>
   )
 }
 
