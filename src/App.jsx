9853d78575cff87e3c264dae00a0454d471b0fe4
morteza <mgahangir2006@gmail.com>
2023-03-07 13:41:49 +0330

    Completed_Course

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,25 @@ import styles from './style';
 
 import {Navbar, Hero, Stats, Business, Billing, CardDeal ,
         Testimonials, Clients ,CTA ,Footer } from './components';
-//  Bug when HMR Error at Vite version 4.1.0 
-export const u = 12
-console.log('exporting u really screws up HMR :(', u)
+
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={'${styles.paddingX} ${styles.flexCenter}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
             <Navbar />
         </div>
       </div>
-
-      <div className={'bg-primary ${styles.flexStart}'}>
-        <div className={'${styles.boxWidth}'}>
+ 
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
 
-      <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
-        <div className={'${styles.boxWidth}'}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
           <Billing />
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal<br className='sm:block
+        hidden'/> in few easy setps.
+      </h2>
+      <p className={`${styles.paragraph}
+                    max-w-[470px] mt-5`}> 
+        Arcu torelkdslfj, adsio iopjdspioh osdhf0e ioh 
+        dfaiojoidfo odfi di lsdfi l.D opiodifl sdofhjoij 
+        jljdf oiheif oeihfoi dfoihe oiehfoeiofhe.C Ali 
+        ohf oihsdf ax, poidhfioh.
+      </p>
+      <Button styles="mt-10"/>
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img 
+        src={card} 
+        alt="card"
+        className='w-[100%] h-[100%]' 
+      />
+    </div>
+  </section>
+)
 
 export default CardDeal
